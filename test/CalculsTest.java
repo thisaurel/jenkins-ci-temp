606c8a5a842162090ed556c41fd9dc1c0047d701
Aurélien Riche <contact@aurelienriche.com>
2021-01-04 15:00:17 +0100

    add fail UT

---
+++ test/CalculsTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.jnuit.Test;
+
+public class CalculsTest {
+    @Test
+    public void testMultiplier() {
+        failt('not yet implemented');
+    }
+}
